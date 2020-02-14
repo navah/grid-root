a086c515c4ae3cf6ad60db949d8bd7199399708c
Your Name <navahnavahnavah@gmail.com>
2020-02-13 17:33:48 -0700

    started my fav jQuery bits, might switch to React, but using jQuery for now

---
+++ scripts.js
@@ -0,0 +1,16 @@
+function show(id) {
+// selected off tabs, then on a tab
+$('.tab').removeClass('selected').filter(function () {
+return (this.hash === id);
+}).addClass('selected');
+
+// hide and selectively filter an individual panel
+$('.panel').hide().filter(id).show();
+}
+
+$(window).on('hashchange', function () {
+show(location.hash);
+});
+
+// start with the first tab (temporary, obvi)
+show('#tab-the-first');
