6b03eb261c52dcd66188bba0b293726b63ded3ee
Your Name <navahnavahnavah@gmail.com>
2020-02-20 16:15:10 -0700

    bundling element shading

--- scripts.js
+++ scripts.js
@@ -22,6 +22,35 @@ $(window).on('hashchange', function () {
 show(location.hash);
 });
 
+
+
+// hashchange makes tab colors shade and unshade
+$(window).on('hashchange', function () {
+    if (location.hash === "#tab-the-first") {
+        $('.mid').removeClass('shaded-tab');
+        $('.mid-1').addClass('shaded-tab');
+    }
+
+    else if (location.hash === "#tab-the-second") {
+        $('.mid').removeClass('shaded-tab');
+        $('.mid-2').addClass('shaded-tab');
+    }
+
+    else if (location.hash === "#tab-the-third") {
+        $('.mid').removeClass('shaded-tab');
+        $('.mid-3').addClass('shaded-tab');
+    }
+
+    else if (location.hash === "#tab-the-fourth") {
+        $('.mid').removeClass('shaded-tab');
+        $('.mid-4').addClass('shaded-tab');
+    }
+
+});
+
+
+
+
 // start with the first tab (temporary, obvi)
 show('#tab-init');
 
